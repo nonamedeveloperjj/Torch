67834437ebae0f002af22d7f5e1abbdc9df3d877
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2023-04-03 20:57:14 +0300

    Initial Commit

---
+++ Torch/TorchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorchApp.swift
+//  Torch
+//
+//  Created by John Snow on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TorchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
