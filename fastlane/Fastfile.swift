0cf73b0b470e8da8f4ff8ac1f862898826e95b4a
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2023-04-04 12:14:42 +0300

    Add fastlane support

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,25 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+	func runTestsLane() {
+        desc("Build project and run unit tests")
+        scan(
+            scheme: "TorchTests",
+            devices: .userDefined(["iPhone 14"]),
+            skipDetectDevices: true,
+            codeCoverage: .userDefined(true),
+            buildlogPath: "./fastlane/fastlane-buildlog",
+            derivedDataPath: "./DerivedData",
+            sdk: "iphoneos",
+            configuration: "Debug"
+        )
+	}
+}
